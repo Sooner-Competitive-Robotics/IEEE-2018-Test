d79c52364ad0f10912aeaea43310f309e27cc3bd
jkleiber <jkleiber8@gmail.com>
2018-02-26 16:55:52 -0600

    Fixes gyro read error

--- DrivetrainDriveTest/DrivetrainDriveTest.ino
+++ DrivetrainDriveTest/DrivetrainDriveTest.ino
@@ -1,8 +1,6 @@
 #include <RobotLib.h>
 #include <IEEErobot2018.h>
-
-Drivetrain drivetrain;
-Intake intake;						
+				
 
 //Drive Status Flag
 bool driveComplete = false;
@@ -15,83 +13,19 @@ int index = 0;
 //Debug vars
 float left, right, dist;
 
-void encLeftInterrupt() 
-{
-	drivetrain.getLeftEncoder().process(); 
-}
-  
-void encRightInterrupt() 
-{  
-	drivetrain.getRightEncoder().process(); 
-}
 
 void setup() 
 {	
-	Serial.begin(9600);
-	Serial.print(" -Has Begun- \n");
-	
-	Collection<float> turnK(3);
-	Collection<float> distanceK(3);
-	
-	//Add Turn Constants
-	turnK.add(TURN_KP);
-	turnK.add(TURN_KI);
-	turnK.add(TURN_KD);
-	
-	//Add Distance Constants
-	distanceK.add(DIST_KP);
-	distanceK.add(DIST_KI);
-	distanceK.add(DIST_KD);
-	
-	//--Drivetrain
-	Motor leftMot;
-	Motor rightMot;
-	Encoder leftEnc(pinLeftEnc1, pinLeftEnc2);
-	Encoder rightEnc(pinRightEnc1, pinRightEnc2);
-	DigitalDevice mDetector(pinMetDet, INPUT);
-	IRMatrix mat(pinIRMatrix1, pinIRMatrix2, pinIRMatrix3, pinIRMatrix4, pinIRMatrix5);
-
-	//Set the encoder constants
-	leftEnc.setConstant(rightEncoderConstant);
-	rightEnc.setConstant(rightEncoderConstant);
-
-	//--Drivetrain
-	leftMot.begin(pinLeftMot1, pinLeftMot2, pinLeftMotEnb);
-	rightMot.begin(pinRightMot1, pinRightMot2, pinRightMotEnb);
-	drivetrain.begin(leftMot, rightMot, leftEnc, rightEnc, mat, mDetector);
-	
-	//Setup PID
-	drivetrain.initializeTurnPID(turnK);
-	drivetrain.initializeDistancePID(distanceK);
-	
-	Serial.print(" -Drive Has Begun- \n");
-	
-	//--Intake
-	Encoder tEncoder(pinIntakeEnc1, pinIntakeEnc2);
-	DigitalDevice lSwitch(pinLimSwitch, INPUT);
-	Electromagnet eMagnet(pinElecMag);
-	Motor iMotor;	
-	Turntable turntable(turntableServoPin);
-	
-	//--Intake
-	iMotor.begin(pinIbtakeMot1, pinIbtakeMot2, pinIbtakeMotEnb);
-	eMagnet.initialize(pinElecMag);
-	intake.begin(iMotor, tEncoder, mDetector, lSwitch, eMagnet, turntable, colorServoPin);
-  
-	Serial.print(" -Intake Has Begun- \n");
-	
-	//--Interrupts
-	attachInterrupt(0, encLeftInterrupt, CHANGE);
-	attachInterrupt(1, encRightInterrupt, CHANGE);
-	
-	//Interrupt for Turntable Encoder needed + method
-	Serial.print(" -Interrupts- \n");
+	robotSetup();
 }
 
 void loop()
 {	
 	if(!driveComplete)
 	{
+		Serial.print("Drive Command ID: ");
+		Serial.print(index);
+		
 		updateGyro();
 		
 		Serial.print(" \tYaw: ");
@@ -110,11 +44,16 @@ void loop()
 		Serial.print("\tR:");
 		Serial.print(right);
 		Serial.print("\tDIST: ");
-		Serial.println(dist);
+		Serial.print(dist);
 		
 		//Drive 12 inches straight
 		driveComplete = drivetrain.drive(distances[index], angles[index], yaw, resetDrive);
 		resetDrive= false;
+		
+		Serial.print("\tStatus: ");
+		Serial.print(driveComplete);
+		
+		Serial.print("\n");
 	}
 	else
 	{

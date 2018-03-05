04c1908d99f5f02a99b1bdab425cea438d0a27cc
Logan Elliott <logan.a.elliott-1@ou.edu>
2018-03-05 17:57:02 -0600

    fixed test

--- ServoTest/ServoTest.ino
+++ ServoTest/ServoTest.ino
@@ -1,16 +1,40 @@
 #include <Servo.h>
 
 Servo servo;
-
+int userInput;
 void setup()
 {
 	servo.attach(7);
+  Serial.begin(9600);
+  servo.write(0);
+  
+  //Prompt user
+  Serial.println("Enter an integer value for angle 0 to 180");
+  
 }
 
 void loop()
 {
-	servo.write(180);
-	delay(2000);
-	servo.write(0);
-	delay(2000);
+
+  //Is there anything new on the Serial buffer?
+  if(Serial.available() > 0)
+  {
+    //Read user input
+    userInput = Serial.parseInt();
+    
+    //Constrain to -180 - 180, where 100 is max forward output and -100 is backwards full speed
+    userInput = userInput > 180 ? 180 : userInput;
+    userInput = userInput < 0 ? -0 : userInput;
+
+    servo.write(userInput);
+    
+    //Indicate output
+    Serial.print("Output: ");
+    Serial.println(userInput);
+    Serial.println();
+    
+    //Prompt user again
+    Serial.println("Enter an integer value for angle 0 to 180");
+  }
+  delay(20);
 }

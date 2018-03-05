dfa842658fb3f32256483ce1303230b29d7decb4
jkleiber <jkleiber8@gmail.com>
2018-03-05 15:02:57 -0600

    Makes a motor tester that reads Serial

---
+++ MotorTest/MotorTest.ino
@@ -0,0 +1,51 @@
+#include <RobotLib.h>
+
+#define PIN_A 22
+#define PIN_B 23
+#define PIN_EN 13
+
+Motor motor;
+float out = 0.0;
+int userInput = 0;
+
+void setup()
+{
+	//Start the Serial
+	Serial.begin(9600);
+	
+	//Setup the motor
+	motor.begin(PIN_A, PIN_B, PIN_EN);
+	
+	//Prompt user
+	Serial.println("Enter an integer value for % speed (-100 to 100):");
+}
+
+void loop()
+{
+	//Is there anything new on the Serial buffer?
+	if(Serial.available() > 0)
+	{
+		//Read user input
+		userInput = Serial.parseInt();
+		
+		//Constrain to -100 - 100, where 100 is max forward output and -100 is backwards full speed
+		userInput = userInput > 100 ? 100 : userInput;
+		userInput = userInput < -100 ? -100 : userInput;
+		
+		//Calculate output
+		out = userInput / 100.;
+		
+		//Set the motor's output
+		motor.output(out);
+		
+		//Indicate output
+		Serial.print("Output: ");
+		Serial.println(out);
+		Serial.println();
+		
+		//Prompt user again
+		Serial.println("Enter an integer value for % speed (-100 to 100):");
+	}
+	
+	delay(20);
+}

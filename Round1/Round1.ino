cf575dfda1db9cd4d83bb45d2dbce727b680f9af
Hambonaire <32001455+Hambonaire@users.noreply.github.com>
2018-03-12 18:41:36 -0500

    Fixes Round1 so it can complie

--- Round1/Round1.ino
+++ Round1/Round1.ino
@@ -21,121 +21,130 @@ void setup()
 
 void loop() {
 	// put your main code here, to run repeatedly:
-	drivetrain.drive(17, 0);
-	drivetrain.searchForward();
+	drivetrain.drive(17, 0, yaw, resetDrive);
+	drivetrain.searchForward(yaw);
 
-	drivetrain.turnToAngle(90);
+	drivetrain.drive(0, 90, yaw, resetDrive);
 	drivetrain.followLineUntilCoin();                         //need to modify method because robot will drive through gray square where there is no line
 	if(intake.coinDetected())
 	{
-		drivetrain.drive(distMetalDetectToIntake, 0);
-		intake.pickUpSequenceA();
+		drivetrain.drive(distMetalDetectToIntake, 0, yaw, resetDrive);
+		updateColorSensor();
+		intake.pickUpSequence(currentColor);
 		coinCount++;
 	}
-	drivetrain.drive(distIntakeToMatrix, 0);          //on bottom right 4' sq
-	drivetrain.turnToAngle(-135);               //turn onto diagonal (from top left to bottom right)
+	drivetrain.drive(distIntakeToMatrix, 0, yaw, resetDrive);          //on bottom right 4' sq
+	drivetrain.drive(0, -135, yaw, resetDrive);               //turn onto diagonal (from top left to bottom right)
   
 	while(coinCount != 2)                   //follow diagonal and pick up coins
 	{
 		drivetrain.followLineUntilCoin();
 		if(intake.coinDetected())
 		{
-			drivetrain.drive(distMetalDetectToIntake, 0);
-			intake.pickUpSequenceA();
+			drivetrain.drive(distMetalDetectToIntake, 0, yaw, resetDrive);
+			updateColorSensor();
+			intake.pickUpSequence(currentColor);
 			coinCount++;
 		}  
 	}                             			//end on bottom left 2' sq
   
-	drivetrain.drive(34, 0);					//jump across gray square
+	drivetrain.drive(34, 0, yaw, resetDrive);					//jump across gray square
 	
 	while(coinCount != 4)                   //follow diagonal and pick up coins
 	{
 		drivetrain.followLineUntilCoin();
 		if(intake.coinDetected())
 		{
-		drivetrain.drive(distMetalDetectToIntake, 0);
-		intake.pickUpSequenceA();
+		drivetrain.drive(distMetalDetectToIntake, 0, yaw, resetDrive);
+		updateColorSensor();
+		intake.pickUpSequence(currentColor);
 		coinCount++;
 		}  
 	}                             //end on top left 4' sq
   
-	drivetrain.drive(distIntakeToMatrix, 0);
-	drivetrain.turnToAngle(-135);               //turn to face down on left side 4' sq
+	drivetrain.drive(distIntakeToMatrix, 0, yaw, resetDrive);
+	drivetrain.drive(0, -135, yaw, resetDrive);               //turn to face down on left side 4' sq
 
 	while(coinCount != 6)                   //follow left side 4' sq down 
 	{
 		drivetrain.followLineUntilCoin();
 		if(intake.coinDetected())
 		{
-		drivetrain.drive(distMetalDetectToIntake, 0);
-		intake.pickUpSequenceA();
+		drivetrain.drive(distMetalDetectToIntake, 0, yaw, resetDrive);
+		updateColorSensor();
+		intake.pickUpSequence(currentColor);
 		coinCount++;
 		}  
 	}                             //ends on bottom left 4' corner
   
-	drivetrain.drive(distIntakeToMatrix, 0);
-	drivetrain.turnToAngle(-135);               //turn to diagonal (from bottom left to top right)
+	drivetrain.drive(distIntakeToMatrix, 0, yaw, resetDrive);
+	drivetrain.drive(0, -135, yaw, resetDrive);               //turn to diagonal (from bottom left to top right)
 
 	while(coinCount != 7)                   //follow diagonal and pick up coins
 	{
 		drivetrain.followLineUntilCoin();
 		if(intake.coinDetected())
 		{
-			drivetrain.drive(distMetalDetectToIntake, 0);
-			intake.pickUpSequenceA();
+			drivetrain.drive(distMetalDetectToIntake, 0, yaw, resetDrive);
+			updateColorSensor();
+			intake.pickUpSequence(currentColor);
 			coinCount++;
 		}  
 	}                             //ends on bottom left 2' corner
   
-	drivetrain.drive(34, 0);					//jump across the gray square
+	drivetrain.drive(34, 0, yaw, resetDrive);					//jump across the gray square
   
 	while(coinCount != 9)                   //follow diagonal and pick up coins
 	{
 		drivetrain.followLineUntilCoin();
 		if(intake.coinDetected())
 		{
-			drivetrain.drive(distMetalDetectToIntake, 0);
-			intake.pickUpSequenceA();
+			drivetrain.drive(distMetalDetectToIntake, 0, yaw, resetDrive);
+			updateColorSensor();
+			intake.pickUpSequence(currentColor);
 			coinCount++;
 		}  
 	}                             //ends on top right 4' corner
   
-	drivetrain.drive(distIntakeToMatrix, 0);
-	drivetrain.turnToAngle(135);                //turn to face down on right side 6' sq
+	drivetrain.drive(distIntakeToMatrix, 0, yaw, resetDrive);
+	drivetrain.drive(0, 135, yaw, resetDrive);                //turn to face down on right side 6' sq
 
 	while(coinCount != 10)                  //follow right side 4' sq down
 	{
 		drivetrain.followLineUntilCoin();
 		if(intake.coinDetected())
 		{
-			drivetrain.drive(distMetalDetectToIntake, 0);
-			intake.pickUpSequenceA();
+			drivetrain.drive(distMetalDetectToIntake, 0, yaw, resetDrive);
+			updateColorSensor();
+			intake.pickUpSequence(currentColor);
 			coinCount++;
 		}  
 	}                             		//ends on middle right 4' sq
-	drivetrain.drive(distIntakeToMatrix, 0);
-	drivetrain.turnToAngle(90);               //turn to face left on middle of right side 6' sq
+	drivetrain.drive(distIntakeToMatrix, 0, yaw, resetDrive);
+	drivetrain.drive(0, 90, yaw, resetDrive);               //turn to face left on middle of right side 6' sq
 
 	while(coinCount != 11)                  //follow the middle line to the left
 	{
 		drivetrain.followLineUntilCoin();
 		if(intake.coinDetected())
 		{
-			drivetrain.drive(distMetalDetectToIntake, 0);
-			intake.pickUpSequenceA();
+			drivetrain.drive(distMetalDetectToIntake, 0, yaw, resetDrive);
+			updateColorSensor();
+			intake.pickUpSequence(currentColor);
 			coinCount++;
 		}  
 	}
 
-	drivetrain.drive(23, 0);				//jump across the gray square
+	drivetrain.drive(23, 0, yaw, resetDrive);				//jump across the gray square
 
 	while(coinCount != 12)                  //follow the middle line to the left
 	{
 		drivetrain.followLineUntilCoin();
 		if(intake.coinDetected())
 		{
-			drivetrain.drive(distMetalDetectToIntake, 0);
-			intake.pickUpSequenceA();
+			drivetrain.drive(distMetalDetectToIntake, 0, yaw, resetDrive);
+			updateColorSensor();
+			intake.pickUpSequence(currentColor);
 			coinCount++;
 		}  
 	}
@@ -145,35 +154,35 @@ void loop() {
 	//drive till reach color and drive to all colors and go back to white square
   
 	/*
-	drivetrain.turnToAngle(180); 
+	drivetrain.drive(180); 
 	drivetrain.drive(12, 0);  
 	intake.dropOffSequence(gray); 
-	drivetrain.turnToAngle(180);
+	drivetrain.drive(180);
 	*/
 
-	drivetrain.drive(30, 0);                                 //3.5' is distance from gray sq of color square, DRIVING to GREEN SQUARE
+	drivetrain.drive(30, 0, yaw, resetDrive);                                 //3.5' is distance from gray sq of color square, DRIVING to GREEN SQUARE
 	intake.dropOffSequence(green);    
 
-	drivetrain.turnToAngle(90);
-	drivetrain.drive(42, 0);                                               //3.5' is distance between centers of color squares, DRVING to RED
+	drivetrain.drive(0, 90, yaw, resetDrive);
+	drivetrain.drive(42, 0, yaw, resetDrive);                                               //3.5' is distance between centers of color squares, DRVING to RED
 	intake.dropOffSequence(red);
 
-	drivetrain.turnToAngle(90);
-	drivetrain.drive(84, 0);                                               //7' is distance between centers of color squares (skipping WHITE), DRVING to CYAN
+	drivetrain.drive(0, 90, yaw, resetDrive);
+	drivetrain.drive(84, 0, yaw, resetDrive);                                               //7' is distance between centers of color squares (skipping WHITE), DRVING to CYAN
 	intake.dropOffSequence(cyan);
   
-	drivetrain.turnToAngle(90);
-	drivetrain.drive(42, 0);                                               //3.5' is distance between centers of color squares, DRVING to PURPLE
-	intake.dropOffSequence(purple);
+	drivetrain.drive(0, 90, yaw, resetDrive);
+	drivetrain.drive(42, 0, yaw, resetDrive);                                               //3.5' is distance between centers of color squares, DRVING to PURPLE
+	intake.dropOffSequence(magenta);
   
-	drivetrain.drive(42, 0);                                               //3.5' is distance between centers of color squares, DRVING to YELLOW
+	drivetrain.drive(42, 0, yaw, resetDrive);                                               //3.5' is distance between centers of color squares, DRVING to YELLOW
 	intake.dropOffSequence(yellow);
   
-	drivetrain.turnToAngle(90);
-	drivetrain.drive(84, 0);                                               //7' is distance between centers of color squares (skipping WHITE), DRVING to BLUE
+	drivetrain.drive(0, 90, yaw, resetDrive);
+	drivetrain.drive(84, 0, yaw, resetDrive);                                               //7' is distance between centers of color squares (skipping WHITE), DRVING to BLUE
 	intake.dropOffSequence(blue);
   
-	drivetrain.turnToAngle(180);
-	drivetrain.drive(42, 0);                                               //3.5' is distance between centers of color squares, RETURN to WHITE SQUARE
+	drivetrain.drive(0, 180, yaw, resetDrive);
+	drivetrain.drive(42, 0, yaw, resetDrive);                                               //3.5' is distance between centers of color squares, RETURN to WHITE SQUARE
   
 }

70fe17759035924f6a6505e8d67265974bc279e8
jkleiber <jkleiber8@gmail.com>
2018-03-16 11:54:24 -0500

    Works on turntable servo to no avail

--- ServoTest/ServoTest.ino
+++ ServoTest/ServoTest.ino
@@ -1,16 +1,24 @@
 #include <Servo.h>
 
 Servo servo;
+int angle = 1000;
 
 void setup()
 {
 	servo.attach(7);
+	Serial.begin(9600);
 }
 
 void loop()
 {
-	servo.write(180);
-	delay(2000);
-	servo.write(0);
-	delay(2000);
+	if(Serial.available() > 0)
+	{
+		angle = Serial.parseInt();
+
+		Serial.print("Writing to angle: ");
+		Serial.println(angle);
+	}
+	
+	servo.writeMicroseconds(angle);
+	delay(50);
 }
